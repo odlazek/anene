9e76efa67eaa04409379814932c7cc40c67000d6
odlazek <60104777+odlazek@users.noreply.github.com>
2020-02-13 10:43:11 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,9 @@
+function showMenu() {
+	let meni = document.getElementById("mobileLink");
+	if( meni.style.display === "block"){
+		meni.style.display = "none";
+	}else{
+		meni.style.display = "block";
+	}
+}
+
